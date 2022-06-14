07565b1eda9ad6ed78bf5911650e5ea59cf300ca
AAngel777 <105814810+AAngel777@users.noreply.github.com>
2022-06-13 22:31:40 -0700

    Programa de regularizacion

---
+++ Regularizacion/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Regularizacion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try 
+            {
+                Console.Write("Ingrese el nombre del producto: ");
+                string producto = Console.ReadLine();
+
+                Console.Write("Ingrese la cantidad que desea comprar: ");
+                long cantidad = long.Parse(Console.ReadLine());
+
+                Console.Write("Ingrese el precio del producto: ");
+                float precio = float.Parse(Console.ReadLine());
+
+                clase1 o1 = new clase1(producto, cantidad, precio); // creacion del objeto
+                o1.escribe();
+                o1.datos();    // ejecucion de metodos
+
+                Console.WriteLine("Pulse enter para salir...");
+                Console.ReadKey();
+            }
+            catch(FormatException e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+                Console.WriteLine("Ruta: " + e.StackTrace);  
+                Console.WriteLine("Pulse enter para continuar...");
+                Console.ReadKey();
+
+            }
+
+
+        }
+    }
+}
---
+++ Regularizacion/clase1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Regularizacion
+{
+    class clase1
+    {
+        public string producto;      // atributos
+        public long cantidad;
+        public float precio;
+        public clase1(string producto, long cantidad, float precio) // constructor que recibe parametros y los asigna con
+        {
+            this.producto = producto;
+            this.cantidad = cantidad; // clausula this
+            this.precio = precio;
+        }
+        
+        public void escribe()
+        {
+            try
+            {
+                StreamWriter sw = new StreamWriter("Producto.txt", true); // creacion del objeto StreamWriter
+                sw.WriteLine(producto);
+                sw.WriteLine(cantidad);
+                sw.WriteLine(precio);   // escritura en el archivo
+                sw.Close(); // se cierra archivo
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+ 
+        }
+
+        public void datos()
+        {
+            Console.WriteLine("DATOS DEL PRODUCTO"); 
+            Console.WriteLine("Producto: " + producto);
+            Console.WriteLine("Cantidad: " + cantidad);
+            Console.WriteLine("Precio: {0:c} " , precio);
+        }
+    }
+}
